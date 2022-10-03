9e8b0f0404465c0afd1556d98b1fbacdab1dca22
uzun <hyjhyj0901@pusan.ac.kr>
2022-10-03 18:24:52 +0900

    [refactor] refactor nnoonjy's code
    #9 꼭 확인하기

--- app/src/main/java/com/apptive/easywine/presentation/components/SurveyButtons.kt
+++ app/src/main/java/com/apptive/easywine/presentation/components/SurveyButtons.kt
@@ -1,21 +1,15 @@
 package com.apptive.easywine.presentation.components
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,145 +18,93 @@ import androidx.compose.ui.unit.sp
 import com.apptive.easywine.R
 import com.apptive.easywine.ui.theme.gray_button_before
 import com.apptive.easywine.ui.theme.wine_button
-import com.apptive.easywine.ui.theme.wine_main
-import okhttp3.internal.wait
 import androidx.compose.ui.graphics.Color as Color
 
 
 @Composable
-fun BeforeResultButton() {
-	val shape = RoundedCornerShape(70.dp)
-	Button(
-		onClick = { /*TODO*/ },
-		shape = shape,
-		colors = ButtonDefaults.buttonColors(
-			backgroundColor = gray_button_before
-		),
-		modifier = Modifier
-            .width(280.dp)
-            .height(60.dp)
-	) {
-		Box(
+fun ResultButton(
+	text: String,
+	color: Color,
+	onClick: () -> Unit = {},
+) {
+	val shape =
+		Button(
+			onClick = onClick,
+			colors = ButtonDefaults.buttonColors(
+				backgroundColor = gray_button_before
+			),
 			modifier = Modifier
-                .width(266.dp)
-                .height(50.dp)
-                .border(
-                    width = 1.dp,
-                    color = Color.White,
-                    shape = shape
-                )
+				.width(280.dp)
+				.height(60.dp),
+			shape = RoundedCornerShape(70.dp)
+
 		) {
-			Text(
-				text = "오늘의 와인 확인하기",
-				color = Color.White,
-				fontSize = 16.sp,
-				fontWeight = FontWeight.ExtraBold,
+			Box(
 				modifier = Modifier
-					.align(Alignment.Center)
-			)
+					.width(266.dp)
+					.height(50.dp)
+					.border(
+						width = 1.dp,
+						color = Color.White,
+						shape = RoundedCornerShape(70.dp)
+					)
+			) {
+				Text(
+					text = text,
+					color = Color.White,
+					fontSize = 16.sp,
+					fontWeight = FontWeight.ExtraBold,
+					modifier = Modifier
+						.align(Alignment.Center)
+				)
+			}
 		}
-	}
 }
 
 @Composable
-fun AfterResultButton() {
-	val shape = RoundedCornerShape(70.dp)
+fun PageNavText(
+	onClick: () -> Unit = {},
+	isLeft: Boolean = true
+) {
 	Button(
-		onClick = { /*TODO*/ },
-		shape = shape,
-		colors = ButtonDefaults.buttonColors(
-			backgroundColor = wine_button
-		),
-		modifier = Modifier
-            .width(280.dp)
-            .height(60.dp)
-	) {
-		Box(
-			modifier = Modifier
-                .width(266.dp)
-                .height(50.dp)
-                .border(
-                    width = 1.dp,
-                    color = Color.White,
-                    shape = shape
-                )
-		) {
-			Text(
-				text = "오늘의 와인 확인하기",
-				color = Color.White,
-				fontSize = 16.sp,
-				fontWeight = FontWeight.ExtraBold,
-				modifier = Modifier
-					.align(Alignment.Center)
-
-			)
-		}
-
-	}
-}
-
-
-@Composable
-fun PreviousPage() {
-	Button(
-		onClick = { /*TODO*/ },
-
+		onClick = onClick,
 		colors = ButtonDefaults.buttonColors(
 			backgroundColor = Color.Transparent
 		)
 	) {
 		Row() {
 			Image(
-				painterResource(id = R.drawable.page_previous),
-				contentDescription = "previous page button",
+				painterResource(
+					id = if (isLeft) R.drawable.page_previous else R.drawable.page_previous
+				),
+				contentDescription = "page nav button",
 				modifier = Modifier
-                    .height(22.dp)
-                    .width(22.dp)
+					.height(22.dp)
+					.width(22.dp)
 			)
 			Text(
-				text = "이전으로 돌아가기",
+				text = if (isLeft) "이전으로 돌아가기" else "다음으로 넘어가기",
 				color = gray_button_before,
 				fontSize = 15.sp
 			)
 		}
-
 	}
 }
 
-
-@Composable
-fun NextPage() {
-	Button(
-		onClick = { /*TODO*/ },
-
-		colors = ButtonDefaults.buttonColors(
-			backgroundColor = Color.Transparent
-		)
-	) {
-		Row() {
-			Text(
-				text = "다음으로 넘어가기",
-				color = gray_button_before,
-				fontSize = 15.sp
-			)
-			Image(
-				painterResource(id = R.drawable.page_next),
-				contentDescription = "next page button",
-				modifier = Modifier
-                    .height(22.dp)
-                    .width(22.dp)
-			)
-		}
-	}
-}
 
 @Preview
 @Composable
 fun PreviewResultButton() {
 	Column {
-		BeforeResultButton()
-		AfterResultButton()
-		PreviousPage()
-		NextPage()
+		ResultButton(
+			text = "오늘의 와인 확인하기",
+			color = gray_button_before
+		)
+		ResultButton(
+			text = "오늘의 와인 확인하기",
+			color = wine_button
+		)
+		PageNavText(isLeft = true)
+		PageNavText(isLeft = false)
 	}
 }
