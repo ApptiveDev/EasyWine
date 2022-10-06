251ac9c7a51785c7c37b5f40ec1aae03ac21e62b
차지원 <wona825@naver.com>
2022-10-07 01:54:15 +0900

    [feat] make SurveyScreen
    SurveyScreen 기본화면 구현

--- app/src/main/java/com/apptive/easywine/presentation/Survey/SurveyScreen.kt
+++ app/src/main/java/com/apptive/easywine/presentation/Survey/SurveyScreen.kt
@@ -3,6 +3,8 @@ package com.apptive.easywine.presentation.Survey
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -16,82 +18,66 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apptive.easywine.R
+import com.apptive.easywine.presentation.Home.QCard
 import com.apptive.easywine.presentation.components.*
 import com.apptive.easywine.ui.theme.gray_button_before
 
-
 @Composable
 fun SurveyScreen(){
-    val scrollState = rememberScrollState()
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .background(Color.White)
-            .fillMaxSize()
-            .verticalScroll(scrollState)
-    ) {
-        Box() {
-
-            TextTopBar(title = "오늘의 와인 추천")
-
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(50.dp)
-                    .align(Alignment.TopStart)
-                    .offset(x=15.dp, y=15.dp)
-            ) {
-                Icon(
-                    painterResource(id = R.drawable.survey_arrow),
-                    contentDescription = "previous page button",
-                    modifier = Modifier
-                        .width(42.dp)
-                        .height(48.dp),
-                    tint = Color.White
-                )
-            }
-
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(50.dp)
-                    .align(Alignment.TopEnd)
-                    .offset(x=-15.dp, y=15.dp)
-            ){
-                Icon(
-                    painterResource(id = R.drawable.survey_menu),
-                    contentDescription = "menu page button",
-                    modifier = Modifier
-                        .width(24.dp)
-                        .height(24.dp),
-                    tint = Color.White
-                )
-            }
-
-        }
-        Spacer(modifier = Modifier.size(30.dp))
-        SurveyProgressBar(prog_percent = 0.7f)
-        ///단계별 버튼
-        ///중간에 들어갈 화면
-
-        Box() {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row {
-                    PageNav(isLeft = true)
-                    Spacer(modifier = Modifier.size(30.dp))
-                    PageNav(isLeft = false)
-                }
-                ResultButton(
-                    text = "오늘의 와인 확인하기",
-                    color = gray_button_before
-                )
-            }
-
-        }
-
+            //.fillMaxSize()
+            //.verticalScroll(scrollState)
+    ){
+        TextTopBar(title = "오늘의 와인 추천",18)
+        SurveyTopbar()
+        Spacer(modifier = Modifier.height(30.dp))
+        SurveyTitle("오늘의 와인을 위한 ","바디감 ","질문이에요.")
+        Spacer(modifier = Modifier.height(30.dp))
+        SurveyMainContent()
     }
+
+}
+
+@Composable
+fun SurveyMainContent() {
+    val scrollState = rememberScrollState()
+    Column(){
+        LazyColumn() {
+            item { Spacer(Modifier.size(10.dp)) }
+            repeat(5) {
+                item { SurveyQuestion() }
+            }
+            item {
+                SurveyBottomButton()
+            }
+        }
+    }
+}
+
+@Composable
+fun SurveyBottomButton(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(25.dp))
+        Row {
+            PageNav(isLeft = true)
+            Spacer(modifier = Modifier.size(30.dp))
+            PageNav(isLeft = false)
+        }
+        ResultButton(
+            text = "오늘의 와인 확인하기",
+            color = gray_button_before
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSurveyScreen() {
+    SurveyScreen()
 }
