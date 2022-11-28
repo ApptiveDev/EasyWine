c75ef50e9908279990ac0ad204be6b5878348eb5
차지원 <wona825@naver.com>
2022-11-28 21:52:04 +0900

    [feat] add for wine storage view

--- app/src/main/java/com/apptive/easywine/presentation/viewmodel/DegreeViewModel.kt
+++ app/src/main/java/com/apptive/easywine/presentation/viewmodel/DegreeViewModel.kt
@@ -1,12 +1,14 @@
-package com.apptive.easywine.presentation.viewmodel.mock
+package com.apptive.easywine.presentation.viewmodel
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.apptive.easywine.enums.Degree
+import com.apptive.easywine.enums.Side
 import com.apptive.easywine.presentation.viewmodel.interfaces.IDegreeViewModel
 
 class DegreeViewModel: ViewModel(),
     IDegreeViewModel {
     override var toDegree: MutableState<Degree> = mutableStateOf(Degree.DG_NONE)
+    override var isAll: MutableState<Side> = mutableStateOf(Side.LEFT)
 }
--- app/src/main/java/com/apptive/easywine/presentation/viewmodel/interfaces/IDegreeViewModel.kt
+++ app/src/main/java/com/apptive/easywine/presentation/viewmodel/interfaces/IDegreeViewModel.kt
@@ -2,7 +2,9 @@ package com.apptive.easywine.presentation.viewmodel.interfaces
 
 import androidx.compose.runtime.MutableState
 import com.apptive.easywine.enums.Degree
+import com.apptive.easywine.enums.Side
 
 interface IDegreeViewModel {
     var toDegree: MutableState<Degree>
+    var isAll: MutableState<Side>
 }
