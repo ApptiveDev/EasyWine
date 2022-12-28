0d1bfb7b6556d232571ea8387956b6a269b14ab6
uzun <hyjhyj0901@pusan.ac.kr>
2022-12-28 12:12:16 +0900

    [feat] : api 대통합

--- app/src/main/java/com/apptive/easywine/data/remote/SurveyApi.kt
+++ app/src/main/java/com/apptive/easywine/data/remote/SurveyApi.kt
@@ -1,23 +1,31 @@
 package com.apptive.easywine.data.remote
 
 import com.apptive.easywine.domain.model.Question
+import com.apptive.easywine.domain.model.RecommendBody
+import com.apptive.easywine.domain.model.Wine
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 
 interface SurveyApi {
 
 	// api for requesting questions type 1
-	@GET("/question/category/1")
+	@GET("/question/category/v1/1")
 	suspend fun getQuestion1(
 	): Response<List<Question>>
 
 	// api for requesting questions type 2
-	@GET("/question/category/2")
+	@GET("/question/category/v1/2")
 	suspend fun getQuestion2(
 	): Response<List<Question>>
 
 	// api for requesting questions type 3
-	@GET("/question/category/3")
+	@GET("/question/category/v1/3")
 	suspend fun getQuestion3(
 	): Response<List<Question>>
+
+	@GET("/question/answer/v2")
+	suspend fun getRecommend(
+		@Body recommendBody: RecommendBody
+	): Response<Wine>
 }
